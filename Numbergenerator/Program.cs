9dff881918095d9572c4842419a39b470ab1ed0b
VilgotGredemark <92381767+VilgotGredemark@users.noreply.github.com>
2021-10-12 16:09:00 +0200

    Add files via upload

---
+++ Numbergenerator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Numbergenerator
+{
+    class Program
+    {
+        static void Main(string[] args){   
+            Random rnd = new Random();
+            int nummer =  rnd.Next(1,101);
+
+            Console.WriteLine("Jag tänker på ett nummer mellan 1 och 100, försök att gissa vilket!");
+            
+            int försök = 0;
+            int gissning = 0;
+            while (gissning != nummer) {
+                gissning = Int32.Parse(Console.ReadLine());
+                if (gissning > nummer) {
+                    Console.WriteLine("För högt, gissa igen!");
+
+                } 
+                else if (gissning < nummer){
+                    Console.WriteLine("För lågt, gissa igen!");
+                }
+                försök++; 
+            }
+            if (gissning == nummer) {
+                Console.WriteLine("Du gissade rätt!");
+                Console.WriteLine("Jag tänkte på numret " + nummer + ", det tog dig " + försök + " försök att få rätt svar!");
+            }
+                
+             
+        }
+    }
+}
